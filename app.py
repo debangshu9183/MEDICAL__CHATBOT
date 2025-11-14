ff50426968db9ed078e8a77a28e4a72aa22dc5ce
Debangshu Sadhukhan <debangshumessi@gmail.com>
2025-11-14 23:06:09 +0530

    Fix Flask: disable debug & reloader for EC2

--- app.py
+++ app.py
@@ -32,4 +32,10 @@ def chat():
     return str(bot_response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(
+    host="0.0.0.0",
+    port=5000,
+    debug=False,
+    use_reloader=False)
+
+
